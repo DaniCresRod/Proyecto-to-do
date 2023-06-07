190422dd12c2037b752faabe0cb7f793db48de08
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-06-07 16:30:58 +0200

    Añade OK, modifica OK

--- public/scripts/script_main.js
+++ public/scripts/script_main.js
@@ -1,5 +1,9 @@
+let btnSaveOption=0;
+
 //Toggle "New task" visible (From NavBar New and dialog Buttons)/*
 document.querySelector("nav ul li:nth-of-type(1)").addEventListener("click", ()=>{
+    clearNewTaskDialog();
+    btnSaveOption=dataArray.length;
     document.getElementById("new_task").classList.toggle("invisible");
 });
 
@@ -13,9 +17,15 @@ let dataArray=[];
 //Retrieve InternalStorage information (will be a JSON file, we'll need to parse it into a JS Object)
 if(localStorage.getItem('miTaskList')!==null){
     dataArray=JSON.parse(localStorage.getItem('miTaskList'));
+    btnSaveOption=dataArray.length;
+    refreshTaskView();
 }
 
-console.log(dataArray);
+//Add data to the screen
+function refreshTaskView(){
+    document.getElementById("task_view").innerHTML="";
+    dataArray.forEach(x=>addTasksToDoc(x));
+}
 
 /* -Object Model-
 let toDoTaskObject={
@@ -34,20 +44,14 @@ const input_doneText=document.getElementById("new_task__task_done");
 //Save button funcionality
 document.getElementById("new_task_btns__btnSave").addEventListener("click", ()=>{
     
-    if(input_titleText.value.trim().length > 0){
-        if(!TaskTitlesNotRepeated(input_titleText.value)){
-            dataArray.push({
-                done:input_doneText.checked,
-                title:input_titleText.value,
-                category: input_categoryText.value,
-                task: input_doneText.value
-            });
-            //document.getElementById("new_task").classList.toggle("invisible");
-            localStorage.setItem('miTaskList', JSON.stringify(dataArray))
-        }
-        else{window.alert(`${input_titleText.value} already exists`);}
+    if(btnSaveOption===dataArray.length){
+        btnSaveAdd(dataArray.length);
     }
-    else{window.alert("Your task is meant to have a title at least");}
+    else{
+        btnSaveAdd(btnSaveOption);        
+    }
+    
+    refreshContextMenuListener();
 });
 
 //Checking not equal titles among stored tasks. Returns true on match
@@ -63,4 +67,79 @@ function TaskTitlesNotRepeated(newTaskTitle){
 
     return aMatch;    
 }
+
+//Add a task to the document
+function addTasksToDoc(oneTask){
+    let newTaskContainer = document.createElement("article");
+    let newTaskCheckBox = document.createElement("input");
+        newTaskCheckBox.setAttribute("type", "checkbox");
+        newTaskCheckBox.checked=oneTask.done;
+        newTaskCheckBox.setAttribute("id", oneTask.title);
+    let newTaskTitle = document.createElement("p");
+        newTaskTitle.textContent=oneTask.title;
+    let newTaskCategory = document.createElement("p");
+        newTaskCategory.textContent = oneTask.category;
+    let newTaskDesc = document.createElement("p");
+        newTaskDesc.textContent = oneTask.task;
+
+    let arrayContents=[newTaskCheckBox, newTaskTitle, newTaskCategory, newTaskDesc];
+
+    arrayContents.forEach((eachArrayComponent)=>{
+        newTaskContainer.appendChild(eachArrayComponent);
+    });
+
+    document.getElementById("task_view").appendChild(newTaskContainer);
+}
+
+
+function refreshContextMenuListener(){
+
+    document.querySelectorAll("#task_view article").forEach((x, index)=>{
+
+        x.addEventListener("contextmenu", (event)=>{
+            event.preventDefault();
+    
+            document.getElementById("new_task").classList.toggle("invisible");
+    
+            input_doneText.checked=dataArray[index].done;
+            input_titleText.value=dataArray[index].title;
+            input_categoryText.value=dataArray[index].category;
+            input_taskText.value=dataArray[index].task;
+    
+            btnSaveOption=index;
+        });
+    
+    });
+}
+
+
+
+function btnSaveAdd(whereToSaveIndataArray){
+
+    if(input_titleText.value.trim().length > 0){
+        if(!TaskTitlesNotRepeated(input_titleText.value) || (whereToSaveIndataArray<dataArray.length) ){
+            dataArray[whereToSaveIndataArray]={
+                done:input_doneText.checked,
+                title:input_titleText.value,
+                category: input_categoryText.value,
+                task: input_taskText.value
+            };
+
+            document.getElementById("new_task").classList.toggle("invisible");
+            localStorage.setItem('miTaskList', JSON.stringify(dataArray));
+            refreshTaskView();
+        }
+        else{window.alert(`${input_titleText.value} already exists`);}
+    }
+    else{window.alert("Your task is meant to have a title at least");}
+}
+
+function clearNewTaskDialog(){
+    input_doneText.checked=false;
+        input_titleText.value="";
+        input_categoryText.value="";
+        input_taskText.value="";
+}
+
+
  
